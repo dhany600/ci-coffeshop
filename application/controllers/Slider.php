dfef071d21a082cad2a25a63445980ba0c79f9c8
dhany600 <dhanymartin@gmail.com>
2021-03-28 19:06:01 +0700

    'coffe'

--- application/controllers/Slider.php
+++ application/controllers/Slider.php
@@ -3,11 +3,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Slider extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('M_product');
+	}
 	public function index()
 	{
-		$this->load->view('admin/slider');		
+		$data['row'] = $this->M_product->get_product();
+		$this->load->view('admin/header');
+		$this->load->view('slider/daftar_slider', $data);
+		$this->load->view('admin/footer');
 	}
 
+	public function add()
+	{
+		$this->load->view('admin/header');
+		$this->load->view('slider/add_slider');
+		$this->load->view('admin/footer');
+	}
 }
 
 /* End of file Slider.php */
---
+++ application/views/slider/add_slider.php
@@ -0,0 +1,30 @@
+<section class="content mt-3">
+    <div class="container-fluid">
+        <!-- general form elements -->
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Quick Example</h3>
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form action="ge">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="exampleInputFile">File input</label>
+                        <div class="input-group">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="exampleInputFile">
+                                <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</section>
